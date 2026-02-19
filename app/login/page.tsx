f0e52648d49a5ca4ad2f811dc53e197ebc79350e
Darwin <darwinrj852003@gmail.com>
2026-02-19 14:28:19 +0530

    minor bug: changed localhost:3000/dahshboard to /dashboard

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -6,7 +6,7 @@ export default function LoginPage(){
         await supabase.auth.signInWithOAuth({
             provider: "google",
             options: {
-                redirectTo: "http://localhost:3000/dashboard"
+                redirectTo: "/dashboard"
             }
 });
     }
